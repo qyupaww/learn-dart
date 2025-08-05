4bf03e5f891b4037f8f87e83505472543b8cad74
Faisal Rahman <qyupaww@gmail.com>
2025-08-05 17:38:49 +0700

    dart switch

---
+++ Bab2/ControlFlow/switch.dart
@@ -0,0 +1,30 @@
+void main(List<String> args) {
+  var dayOfWeek = 2;
+
+  switch (dayOfWeek) {
+    case 1:
+      print("Hari Senin");
+      break;
+    case 2:
+      print("Hari Selasa");
+      break;
+    case 3:
+      print("Hari Rabu");
+      break;
+    case 4:
+      print("Hari Kamis");
+      break;
+    case 5:
+      print("Hari Jumat");
+      break;
+    case 6:
+      print("Hari Sabtu");
+      break;
+    case 7:
+      print("Hari Minggu");
+      break;
+    default:
+      print("Invalid");
+      break;
+  }
+}
