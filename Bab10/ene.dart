72fa83619822fba806db31d36850ec9152791254
Faisal Rahman <qyupaww@gmail.com>
2025-08-19 14:44:42 +0700

    dart error n exception

---
+++ Bab10/ene.dart
@@ -0,0 +1,15 @@
+void main() {
+  var value = numDivideByZero(10, 8);
+  print(value);
+
+  var value2 = numDivideByZero(10, 0);
+  print(value2);
+}
+
+num numDivideByZero(int value, int divider) {
+  if (divider == 0) {
+    throw Exception("Can't divide by Zero");
+  } else {
+    return value / divider;
+  }
+}
